acac3d8b1d20aaf3992328f596458ef6af6949b4
Arda <aersoy3535>
2023-09-22 00:46:43 +0300

    minor changes

--- src/Components/Chart.jsx
+++ src/Components/Chart.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import {
   VictoryChart,
   VictoryLegend,
@@ -8,6 +8,7 @@ import {
   VictoryTheme,
   VictoryLabel,
   LineSegment,
+  VictoryZoomContainer,
 } from 'victory'
 import AppContext from '../Context'
 
@@ -19,8 +20,8 @@ const Chart = () => {
 
   const { dilScore, sosyalScore, kabaScore, inceScore, gelisimScore } = useContext(AppContext)
   const { dilScoreTreshold, sosyalScoreTreshold, kabaScoreTreshold, inceScoreTreshold, genelGelisimTreshold } = useContext(AppContext)
-  const { dil20Treshold,sosyal20Treshold,kaba20Treshold,ince20Treshold,
-          genel20Treshold, dil30Treshold, sosyal30Treshold, kaba30Treshold, ince30Treshold, genel30Treshold} = useContext(AppContext)
+  const { dil20Treshold, sosyal20Treshold, kaba20Treshold, ince20Treshold,
+    genel20Treshold, dil30Treshold, sosyal30Treshold, kaba30Treshold, ince30Treshold, genel30Treshold } = useContext(AppContext)
 
 
   const data = [
@@ -32,28 +33,47 @@ const Chart = () => {
   ]
 
   const twentyTresholds = [
-    { name: "Genel Gelisim", pv: genel20Treshold  },
-    { name: 'Dil-Bilişsel', pv:  dil20Treshold},
-    { name: 'İnce Motor', pv:  ince20Treshold},
-    { name: 'Kaba Motor', pv:  kaba20Treshold},
-    { name: 'Sosyal Beceri - Öz Bakım', pv:  sosyal20Treshold}
+    { name: "Genel Gelisim", pv: genel20Treshold },
+    { name: 'Dil-Bilişsel', pv: dil20Treshold },
+    { name: 'İnce Motor', pv: ince20Treshold },
+    { name: 'Kaba Motor', pv: kaba20Treshold },
+    { name: 'Sosyal Beceri - Öz Bakım', pv: sosyal20Treshold }
   ]
 
   const thirtyTresholds = [
-    { name: "Genel Gelisim", pv: genel30Treshold  },
-    { name: 'Dil-Bilişsel', pv:  dil30Treshold},
-    { name: 'İnce Motor', pv:  ince30Treshold},
-    { name: 'Kaba Motor', pv:  kaba30Treshold},
-    { name: 'Sosyal Beceri - Öz Bakım', pv:  sosyal30Treshold}
+    { name: "Genel Gelisim", pv: genel30Treshold },
+    { name: 'Dil-Bilişsel', pv: dil30Treshold },
+    { name: 'İnce Motor', pv: ince30Treshold },
+    { name: 'Kaba Motor', pv: kaba30Treshold },
+    { name: 'Sosyal Beceri - Öz Bakım', pv: sosyal30Treshold }
   ]
+
+  const values = [dilScore, sosyalScore, kabaScore, inceScore, gelisimScore,
+    dilScoreTreshold, sosyalScoreTreshold, kabaScoreTreshold, inceScoreTreshold, genelGelisimTreshold,
+    dil20Treshold, sosyal20Treshold, kaba20Treshold, ince20Treshold,
+    genel20Treshold, dil30Treshold, sosyal30Treshold, kaba30Treshold, ince30Treshold, genel30Treshold
+  ]
+
+  const minValue = Math.min(...values)
+  const maxValue = Math.max(...values)
+
+  useEffect(() => {
+    console.log(minValue, maxValue)
+  }, [])
 
   return (
 
-
-    <VictoryChart width={500} height={400} theme={VictoryTheme.material} animate={{ duration: 1000, onLoad: { duration: 1000, easing: 'bounce' } }}>
-      <VictoryLegend x={180} y={10} orientation="horizontal" gutter={10} data={[
+    
+    <VictoryChart width={500} height={400} theme={VictoryTheme.material}  animate={{ duration: 1000, onLoad: { duration: 1000, easing: 'bounce' } }}>
+      <VictoryLegend x={150} y={10} orientation="horizontal" data={[
         { name: 'Skor', symbol: { fill: 'rgb(33, 68, 85)' } },
+      ]} />
+      <VictoryLegend x={222} y={10} orientation="horizontal" data={[
         { name: 'Hedef', symbol: { fill: 'rgb(44, 205, 185)' } },
+      ]} />
+      <VictoryLegend x={150} y={30} orientation="horizontal" data={[
+        { name: '%20 Yaş', symbol: {fill: 'orange'}},
+        { name: '%30 Yaş', symbol: {fill: 'red'}}
       ]} />
       <VictoryLegend
         x={350}
@@ -61,15 +81,15 @@ const Chart = () => {
         orientation="vertical"
         title={"Hedefler"}
         data={[
-          { name: `Genel Gelişim ${genelGelisimTreshold}`, symbol: { fill: "none" }, labels: {fontSize: 10} },
-          { name: `Dil Bilişsel ${dilScoreTreshold}`, symbol: { fill: "none" }, labels: {fontSize: 10} },
-          { name: `İnce Motor ${inceScoreTreshold}`, symbol: { fill: "none" }, labels: {fontSize: 10} },
-          { name: `Kaba Motor ${kabaScoreTreshold}`, symbol: { fill: "none" }, labels: {fontSize: 10} },
-          { name: `Sosyal Beceri ${sosyalScoreTreshold}`, symbol: { fill: "none" }, labels: {fontSize: 10} },
+          { name: `Genel Gelişim ${genelGelisimTreshold}`, symbol: { fill: "none" }, labels: { fontSize: 10 } },
+          { name: `Dil Bilişsel ${dilScoreTreshold}`, symbol: { fill: "none" }, labels: { fontSize: 10 } },
+          { name: `İnce Motor ${inceScoreTreshold}`, symbol: { fill: "none" }, labels: { fontSize: 10 } },
+          { name: `Kaba Motor ${kabaScoreTreshold}`, symbol: { fill: "none" }, labels: { fontSize: 10 } },
+          { name: `Sosyal Beceri ${sosyalScoreTreshold}`, symbol: { fill: "none" }, labels: { fontSize: 10 } },
         ]}
-        labelComponent={<VictoryLabel dx={-17}/>}
+        labelComponent={<VictoryLabel dx={-17} />}
         style={{
-          title:{fill : 'rgb(44, 205, 185)', fontSize: 14}
+          title: { fill: 'rgb(44, 205, 185)', fontSize: 14 }
         }}
       />
 
@@ -79,7 +99,7 @@ const Chart = () => {
           tickLabels: { fontSize: 8, fill: 'black', angle: -33 },
           grid: { stroke: 'none' }
         }}
-        tickLabelComponent={<VictoryLabel dy={3}/>}
+        tickLabelComponent={<VictoryLabel dy={3} />}
       />
       <VictoryAxis
         dependentAxis
@@ -91,7 +111,7 @@ const Chart = () => {
           tickLabels: { fontSize: 8, fill: 'black' },
           grid: { stroke: 'none' }
         }}
-        domain={[0, 145]}
+        domain={[minValue, maxValue]}
       />
       <VictoryBar
         data={data}
@@ -115,13 +135,13 @@ const Chart = () => {
       }}
       />
 
-      <VictoryLine data={twentyTresholds} x="name" y="pv" style={{
-        data: {stroke: 'orange'}
-      }} />
+      {genel20Treshold === 0 ? null : <VictoryLine data={twentyTresholds} x="name" y="pv" style={{
+        data: { stroke: 'orange' }
+      }} />}
 
-      <VictoryLine data={thirtyTresholds} x="name" y="pv" style={{
-        data: {stroke: 'red'}
-      }} />
+      {genel20Treshold === 0 ? null : <VictoryLine data={thirtyTresholds} x="name" y="pv" style={{
+        data: { stroke: 'red' }
+      }} />}
     </VictoryChart>
   )
 }
